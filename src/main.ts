5a8e39d5c3ee380e55c4bca6f96d668b9f08b701
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-04-21 17:53:24 +0100

    setup graphql and hello world sample

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-
+dotenv.config();
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
