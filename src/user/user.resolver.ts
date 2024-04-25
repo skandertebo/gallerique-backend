80e80f6b6812f619329489da930bbf5161aab3e1
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-04-25 13:36:07 +0100

    added user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { NotFoundException } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UserSchema } from '../auth/schemas/user.schema';
+import { UserService } from './user.service';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => UserSchema)
+  async getUser(@Args('id') id: number): Promise<UserSchema> {
+    const user = await this.userService.findOne(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Query(() => [UserSchema])
+  async getUsers(): Promise<UserSchema[]> {
+    return this.userService.findAll();
+  }
+}
