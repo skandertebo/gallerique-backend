e4783483fe7968356be71dbadf16b92a4525e789
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-05-05 20:04:56 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 dotenv.config();
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
+    cors: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => {
